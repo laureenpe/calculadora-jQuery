675dca8e88e5de3df1a5f6ecfe5c249656583f25
“Laura <laureen.pelaez.isaza@gmail.com>
2017-06-27 21:35:09 -0400

    updated jQuery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    $("input:button").click(function () {
+        btn = $(this).val();
+        pantalla = $("#resultado").val();
+        if (btn == "C") {
 
+        } else {
+            if (btn == "=") {
+                $("#resultado").val(eval(pantalla));
+            } else {
+                $("#resultado").val(pantalla + btn);
+            }
+
+        }
+    })
 });
+
+//Lo que hace $this es seleccionar al elemento que activo el evento, de esta manera hacemos que el evento corra de forma individual en cada uno de los elementos (en este caso, de los botones). Así que lo que hicimos fue decirle a jQuery que cuando se haga clic en el input button corra un evento, pero que este evento corra únicamente en este/this elemento (el elemento que provocó el evento).
+
+//.eval() en cada uno de los elementos (en este caso, de los botones). Así que lo que hicimos fue decirle a jQuery que cuando se haga clic en el input button corra un evento, pero que este evento corra únicamente en este/this elemento (el elemento que provocó el evento).
